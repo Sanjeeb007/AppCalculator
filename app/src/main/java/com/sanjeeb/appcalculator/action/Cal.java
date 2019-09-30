8345c893fa4df345f56a2bc5da37fda35a98c0f5
Sanjeeb Shrestha <sanrestha420@gmail.com>
2019-09-30 13:41:12 +0545

    Initial commit

---
+++ app/src/main/java/com/sanjeeb/appcalculator/action/Cal.java
@@ -0,0 +1,14 @@
+package com.sanjeeb.appcalculator.action;
+
+import com.sanjeeb.appcalculator.model.Numbers;
+
+public interface Cal {
+    public String add(Numbers n);
+
+    public String sub(Numbers n);
+
+    public String divide(Numbers n);
+
+    public String mul(Numbers n);
+
+}
